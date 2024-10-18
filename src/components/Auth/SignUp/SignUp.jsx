3c49c8ce34fd60fe08beeb4ce5e131840cb34920
kartiknoriya <kartiknoriya56@gmail.com>
2024-10-18 18:28:08 +0530

    auth working

--- src/components/Auth/SignUp/SignUp.jsx
+++ src/components/Auth/SignUp/SignUp.jsx
@@ -2,98 +2,104 @@ import React , {useState} from 'react';
 import bg from "../../../assets/bg.mp4";
 import bgpic from "../../../assets/bg.png";
 import { Link } from "react-router-dom";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-
 import "./signUp.css";
 import { useNavigate } from 'react-router-dom';
-import { signInWithPopup , GoogleAuthProvider } from 'firebase/auth';
+import {
+         signInWithPopup , 
+         GoogleAuthProvider , 
+         createUserWithEmailAndPassword 
+        
+        } from 'firebase/auth';
 import { Auth } from '../firebase';
 
 
 
-const SignUp = () => {
+function SignUp() {
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = async (e)=>{
+  const googleprovider = new GoogleAuthProvider();
+
+
+  const handleSubmit = async (e) => {
     console.log(e);
-    
-    try{
-       const userCredential = await createUserWithEmailAndPassword(Auth,email,password);
-       console.log(userCredential);
-       navigate("/");
-    } catch (error){
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(Auth, email, password);
+      console.log(userCredential);
+      navigate("/");
+    } catch (error) {
       console.error(error);
     }
-    if (console.error)  {
-       setErrorMsg("Fill all the fields correctly");
-       return;
+    if (console.error) {
+      setErrorMsg("Fill all the fields correctly");
+      return;
     }
-     setErrorMsg("");
+    setErrorMsg("");
 
 
-  }
-  const handleSingnInWithGoogle = async () =>{
-     
+  };
+  const handleSingnInWithGoogle = async () => {
 
-    try{
+    try {
 
-       const result = await signInWithPopup(Auth , GoogleAuthProvider);
-       console.log(result);
-       navigate("/");
+      await signInWithPopup(Auth, googleprovider);
+      navigate("/");
 
-    } catch (error){
-          console.error(error);
+    } catch (error) {
+      console.error(error);
     }
-  }
-  
-    return (
-        <div className="login-container">
-          <video src={bg} autoPlay loop muted></video>
-    
-          <div className="login-content">
-            
-    
-            <div className="login-section">
-    
-              <div className="login-box">
-    
-                <div  onSubmit={handleSubmit} className="login-box-one">
-                  <h2>Sign Up</h2>
-                  
-                  <input type="text" placeholder="Name" value={name}
-                    onChange ={ (e) => setName(e.target.value)}  />
-                    <input type="email" placeholder="Email" 
-                      onChange={ (e) => setEmail(e.target.value)} />
-                       <input type="password" placeholder="Password" 
-                        onChange={ (e) => setPassword(e.target.value)}/>
-                </div>
-                  
-    
-                <div className="login-box-two">
-                  <p className="border"></p>
-                  <b className="Error-Msg">{errorMsg}</b>
-                  <button onClick={handleSubmit} className="btn" >Sign Up</button>
-                  <button onClick={handleSingnInWithGoogle} className="btn google-btn">Log in with Google</button>
-                  <p className="sign-up-account">
-                    Have an account? <Link to='/login' >login</Link>
-                  </p>
-                </div>
-    
-              </div>
-    
+
+  };
+
+  return (
+    <div className="login-container">
+      <video src={bg} autoPlay loop muted></video>
+
+      <div className="login-content">
+
+
+        <div className="login-section">
+
+          <div className="login-box">
+
+            <div onSubmit={handleSubmit} className="login-box-one">
+              <h2>Sign Up</h2>
+
+              <input type="text" placeholder="Name" value={name}
+                onChange={(e) => setName(e.target.value)} />
+              <input type="email" placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)} />
+              <input type="password" placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)} />
             </div>
 
-            <div className="login-image">
-              <img src={bgpic} alt="#" />
+
+            <div className="login-box-two">
+              <p className="border"></p>
+              <b className="Error-Msg">{errorMsg}</b>
+              <button onClick={handleSubmit} className="btn">Sign Up</button>
+              <button onClick={handleSingnInWithGoogle} className="btn google-btn">Log in with Google</button>
+              <p className="sign-up-account">
+                Have an account? <Link to='/login'>login</Link>
+              </p>
             </div>
 
           </div>
+
         </div>
-      );
+
+        <div className="login-image">
+          <img src={bgpic} alt="#" />
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default SignUp
